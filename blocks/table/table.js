ee1a1bdde59d5eefeba652e2f36d7a6ca609196f
chrisau168@gmail.com <chrisau168@gmail.com>
2023-10-06 14:54:49 +1100

    debugging table

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -2,5 +2,5 @@ import { getMetadata, decorateIcons } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
   // nothing to do here
-    console.log(JSON.stringify(block));
+    console.log('test',JSON.stringify(block));
 }
